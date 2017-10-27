6861b0b835092a4f8e2ed97f66ccf4220ffc46ef
akhayapanigrahi <22980@BLRPRGGWN22980.ITCINFOTECH.com>
2017-10-27 11:20:13 +0530

    Saved Now Demo1 file

---
+++ com.Demo/src/test/java/Demo/com/Demo/Demo1.java
@@ -0,0 +1,15 @@
+package Demo.com.Demo;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hello");
+		
+		System.out.println("Hello Satyajit Samal Local");
+		System.out.println("Changed file");
+
+	}
+
+}
